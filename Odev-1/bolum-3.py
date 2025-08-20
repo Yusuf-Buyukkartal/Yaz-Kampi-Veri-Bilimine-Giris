d2f82368758bae05f43523f98cea1eb0b24b9c25
Yusuf Büyükkartal <yusufbuyukkartal0@gmail.com>
2025-08-20 17:18:40 +0300

    Update bolum-3.py

--- Odev-1/bolum-3.py
+++ Odev-1/bolum-3.py
@@ -1,19 +1,19 @@
 # Bolum 3 - Soru 9
 
-# urun1 = float(input("ilk urunun fiyatini giriniz: "))
-# urun2 = float(input("ikinci urunun fiyatini giriniz: "))
-# urun3 = float(input("ucuncu urunun fiyatini giriniz: "))
+urun1 = float(input("ilk urunun fiyatini giriniz: "))
+urun2 = float(input("ikinci urunun fiyatini giriniz: "))
+urun3 = float(input("ucuncu urunun fiyatini giriniz: "))
 
-# toplam_fiyat = urun1 + urun2 + urun3
+toplam_fiyat = urun1 + urun2 + urun3
 
-# indirim = (toplam_fiyat > 200) * (toplam_fiyat * 0.10)
-
-
-# son_fiyat = toplam_fiyat - indirim
+indirim = (toplam_fiyat > 200) * (toplam_fiyat * 0.10)
 
 
-# print("Toplam fiyat:", toplam_fiyat, "TL")
-# print("Ödenecek tutar:", son_fiyat, "TL")
+son_fiyat = toplam_fiyat - indirim
+
+
+print("Toplam fiyat:", toplam_fiyat, "TL")
+print("Ödenecek tutar:", son_fiyat, "TL")
 
 # Bölüm 3 - Soru 10
 
@@ -26,5 +26,6 @@ sonuc = (yas < 12) * 0 + (13 <= yas <= 17) * 1 + (yas >= 18) * 2
 
 
 mesajlar = ["Çocuksunuz" , "Ergensiniz" , "Yetişkinsiniz"]
+
 
 print(mesajlar[sonuc])
